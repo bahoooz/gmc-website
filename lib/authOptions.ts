56d32a1adb3eeeaae7fc8970af820c9311fac2c2
Bahoz <julienbraultcherif@gmail.com>
2024-05-15 22:01:54 +0200

    fix auth secret

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -18,5 +18,6 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.DISCORD_CLIENT_ID as string,
       clientSecret: process.env.DISCORD_CLIENT_SECRET as string
     })
-  ]
+  ],
+  secret: process.env.SECRET
 };
